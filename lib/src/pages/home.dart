77d177e61e9ede665aa89c6290c88977f83e19cc
Benjamín Ayala <bayala0@ucol.mx>
2021-08-28 12:10:56 -0500

    style(home): font size format

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 class PageHome extends StatelessWidget {
+  // Declaramos una variable para reutilizar el código de los estilos
+  final fnt25 = new TextStyle(fontSize: 25);
+
+  /*
+   * Notas.
+   * 01 - El widget Column, puede recibir un arreglo de widget's
+   * 02 - Esos widget’s que recibe Column, pueden ser cualquier widget
+   * 03 - Con mainAxisAlignment centramos todo el contenido dentro del widget Column 
+   */
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +19,19 @@ class PageHome extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text("Home Cuerpo"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Número de Click's:",
+              style: fnt25,
+            ),
+            Text(
+              "0",
+              style: fnt25,
+            ),
+          ],
+        ),
       ),
     );
   }
